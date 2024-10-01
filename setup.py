4e2ab6d73e365818db0939d19c5cb9bf616d6289
johnson.chen <joy6677@qq.com>
2024-10-01 23:29:17 +0800

    update(setup) setup to pypi

--- setup.py
+++ setup.py
@@ -1,27 +1,43 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='cotkg-network-intrusion-detection',
-    version='0.1',
-    packages=find_packages(exclude=['tests*']),
-    license='MIT',
-    description='A network intrusion detection system using Chain of Thought, knowledge graphs and GraphSAGE',
-    long_description=open('README.md').read(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'scikit-learn',
-        'torch',
-        'torch-geometric',
-        'networkx',
-        'py2neo',
-        'tqdm',
-        'matplotlib',
-        'seaborn',
-        'tsfresh',
-        'shap'
+    name="cotkg-network-intrusion-detection",
+    version="0.1.0",
+    author="Chen Xingqiang",
+    author_email="chen.xingqiang@iechor.com",
+    description="A network intrusion detection system using Chain of Thought, knowledge graphs and GraphSAGE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chenxingqiang/cotkg-network-intrusion-detection",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    url='https://github.com/chenxingqiang/cotkg-network-intrusion-detection',
-    author='Chen Xingqiang',
-    author_email='chen.xingqiang@iechor.com'
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "torch",
+        "torch-geometric",
+        "networkx",
+        "py2neo",
+        "matplotlib",
+        "seaborn",
+        "tsfresh",
+        "shap",
+        "transformers",
+        "openai",
+        "tqdm",
+    ],
 )
